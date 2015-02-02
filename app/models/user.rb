cb80312c7a72d5d3b524fa2b086ae0582a1cda09
MCHLWRRN <me@mchlwrrn.com>
2015-02-01 18:50:10 -0800

    Added Bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ActiveRecord::Base
 	has_many :reverse_subscriptions, foreign_key: :leader_id, class_name: 'Subscription', dependent: :destroy
 	has_many :followers, through: :reverse_subscriptions
 
+	has_many :posts, dependent: :destroy
+	has_many :text_posts, dependent: :destroy
+	has_many :image_posts, dependent: :destroy
+
+	has_many :comments
+
 	def following?(leader)
 		leaders.include? leader
 	end
